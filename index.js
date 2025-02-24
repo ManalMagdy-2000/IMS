c29ef91a9d433eaa444be15605d7412492d7303f
Manal Magdy <manalmagdy092@gmail.com>
2025-02-24 14:27:05 +0200

    add a default route

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const app = require("./api/app");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8000;
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('🚀 API is up and running!');
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
